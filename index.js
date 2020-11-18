0114d4b64bc29a9832bb5da2f8403ba1aaa373be
Bastien <bastien.ratat@gmail.com>
2020-11-18 18:24:33 +0100

    building a single mocha test for register, still need to code a request that pass jwt

--- index.js
+++ index.js
@@ -8,13 +8,31 @@ const authRoutes = require('./routes/auth')
 const usersRoutes = require('./routes/users')
 
 // Connect to database
-mongoose.connect(
-  process.env.DB_CONNECT, 
-  { useNewUrlParser: true, 
-    useUnifiedTopology: true 
-  },
-  () => console.log("Connected to database")
-)
+const env = process.env.NODE_ENV
+
+// Switch environment in the terminal : set NODE_ENV=development
+if (env === 'development') {
+  // Connect to the database for develoment
+  mongoose.connect(
+    process.env.DB_CONNECT, 
+    { useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    },
+    () => console.log("Connected to development database")
+  )
+} else {
+  // Connect to the database for testing
+  mongoose.connect(
+    process.env.DB_TEST, 
+    { useNewUrlParser: true, 
+      useUnifiedTopology: true 
+    },
+    () => console.log("Connected to test database")
+  )
+  mongoose.connection.collections['users'].drop( function(err) {
+    console.log('tests tables are clean for testing');
+  });
+}
 
 // Middleware
 app.use(express.json())
